8b56bac780da8083691f79511fe615a56af13d94
dannys-id <60502767+dannys-id@users.noreply.github.com>
2021-10-15 21:41:33 +0700

    Create bismillah.sh

---
+++ bismillah.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+apt update -y
+apt-get install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential -y
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+cd ccminer && chmod +x build.sh && chmod +x configure.sh && chmod +x autogen.sh && ./build.sh
+./ccminer -a verus -o stratum+tcp://verushash.asia.mine.zergpool.com:3300 -u RW6SnpBXeSv2EX5vnepbpGXukY3BzV6PEB -p  c=VRSC,mc=VRSC,ID=bismillah -t 86
