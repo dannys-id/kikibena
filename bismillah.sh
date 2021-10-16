a915b491f6f72c89bdc08b03673195b9fcb2de3e
dannys-id <60502767+dannys-id@users.noreply.github.com>
2021-10-16 14:08:07 +0700

    Update bismillah.sh

--- bismillah.sh
+++ bismillah.sh
@@ -2,4 +2,5 @@
 wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tgz
 tar -xvf nheqminer-Linux-v0.8.2.tar.gz
+cd nheqminer
 ./nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u RV78iwCVdQZXmVU28ddyRcQUDAChZNggDn -p x c=VRSC mc=VRSC -t 96
