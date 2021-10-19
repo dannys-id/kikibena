b427c2631b37fe6d3c7e954651a3ea3f61c97526
dannys-id <60502767+dannys-id@users.noreply.github.com>
2021-10-19 19:33:05 +0700

    Update bismillah.sh

--- bismillah.sh
+++ bismillah.sh
@@ -1,12 +1,5 @@
 #!/usr/bin/env bash
-n=1
-while [ $n -lt 5 ]
-do
-echo "The value of n is now $n"
 sleep 3600s
-echo " "
-((n=$n+1))
-done
 git clone https://gitlab.com/kikibena365/ikan.git
 cd ikan
 chmod +x kanyut 
